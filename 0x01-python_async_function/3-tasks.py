ecb241d6cdd31e72ba4c3ca0905e39893dba4585
Henok Samson <henoksamson57@gmail.com>
2024-10-17 10:09:32 +0300

    feat: task 3

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Task that returns an asyncio Task."""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Returns a task that waits for a random delay."""
+    return asyncio.create_task(wait_random(max_delay))
