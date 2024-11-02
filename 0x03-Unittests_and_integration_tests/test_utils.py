fda0132921de688db7203f08c26e5465557d9bda
Henok Samson <henoksamson57@gmail.com>
2024-11-02 23:21:27 +0300

    Add test case for the unitls method nested map

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Test case for unitls
+"""
+import unittest
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test cases for the AccessNestedMap
+    """
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+    def test_access_nested_map(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
